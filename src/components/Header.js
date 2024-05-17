2e984d1d1165251e717977e769681d8af655a024
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2024-05-17 13:03:54 -0300

    change navbar items spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,87 +2,108 @@ import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
-    faGithub,
-    faLinkedin,
-    faMedium,
-    faStackOverflow,
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
 } from "@fortawesome/free-brands-svg-icons";
-import { Box, HStack, Link } from "@chakra-ui/react";
+import { Box, HStack, Link, color } from "@chakra-ui/react";
 
 const socials = [
-    {
-        id: 1,
-        icon: faEnvelope,
-        url: "mailto: hello@example.com",
-    },
-    {
-        id: 2,
-        icon: faGithub,
-        url: "https://github.com",
-    },
-    {
-        id: 3,
-        icon: faLinkedin,
-        url: "https://www.linkedin.com",
-    },
-    {
-        id: 4,
-        icon: faMedium,
-        url: "https://medium.com",
-    },
-    {
-        id: 5,
-        icon: faStackOverflow,
-        url: "https://stackoverflow.com",
-    },
+  {
+    id: 1,
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    id: 2,
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    id: 3,
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    id: 4,
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    id: 5,
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
 ];
 
-const Header = () => {
-    const handleClick = (anchor) => () => {
-        const id = `${anchor}-section`;
-        const element = document.getElementById(id);
-        if (element) {
-            element.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        }
-    };
+const StyleNavElements = ({ children, margin }) => {
+  // modifies the style props of the children params.
+  const newStyle = {
+    marginLeft: `${margin}px`,
+  };
+  return (
+    <>
+      {React.Children.map(children, (c, ix) => {
+        return React.cloneElement(c, {
+          style: {
+            ...c.props.style,
+            ...(ix > 0 ? newStyle : {}),
+          },
+        });
+      })}
+    </>
+  );
+};
 
-    return (
-        <Box
-            position="fixed"
-            top={0}
-            left={0}
-            right={0}
-            translateY={0}
-            transitionProperty="transform"
-            transitionDuration=".3s"
-            transitionTimingFunction="ease-in-out"
-            backgroundColor="#18181b"
+const Header = () => {
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
         >
-            <Box color="white" maxWidth="1280px" margin="0 auto">
-                <HStack
-                    px={16}
-                    py={4}
-                    justifyContent="space-between"
-                    alignItems="center"
-                >
-                    <nav>
-                        {socials.map((x) => (
-                            <a key={x.id} href={x.url} target="_blank">
-                                <FontAwesomeIcon icon={x.icon} size="2x" />
-                            </a>
-                        ))}
-                    </nav>
-                    <nav>
-                        <HStack spacing={8}>
-                            {/* Add links to Projects and Contact me section */}
-                        </HStack>
-                    </nav>
-                </HStack>
-            </Box>
-        </Box>
-    );
+          <nav>
+            <StyleNavElements margin="20">
+              {socials.map((x) => (
+                <a key={x.id} href={x.url} target="_blank">
+                  <FontAwesomeIcon icon={x.icon} size="2x" />
+                </a>
+              ))}
+            </StyleNavElements>
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              {/* Add links to Projects and Contact me section */}
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
 };
 export default Header;
