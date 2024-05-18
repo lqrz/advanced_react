f5babc4d6d78ec1e4b0d5ca6e58840b34555c7a6
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2024-05-18 11:33:33 -0300

    display first name on popup

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -12,19 +12,44 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import { useAlertContext } from "../context/alertContext";
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
+  const [submissionData, setSubmissionData] = useState({});
+  const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
+  const url_endpoint = "https://someendpoint.com/post/";
+  useEffect(() => {
+    // send only when the form was submitted.
+    if (Object.keys(submissionData).length > 0) {
+      submit(url_endpoint, submissionData);
+    }
+  }, [submissionData]);
+
+  useEffect(() => {
+    if (response !== undefined && response !== null) {
+      onOpen(response.type, response.message);
+      if (response.type == "success") {
+        formik.resetForm();
+      }
+    }
+  }, [response]);
+
   const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
+    initialValues: { firstName: "", email: "", type: "hireMe", comment: "" },
+    onSubmit: (values) => {
+      setSubmissionData({ ...values });
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().trim().required(),
+      email: Yup.string().trim().email().required(),
+      type: Yup.string().trim().required(),
+      comment: Yup.string().trim().required(),
+    }),
   });
 
   return (
@@ -40,28 +65,44 @@ const LandingSection = () => {
           Contact me
         </Heading>
         <Box p={6} rounded="md" w="100%">
-          <form>
+          <form action="" onSubmit={formik.handleSubmit}>
             <VStack spacing={4}>
-              <FormControl isInvalid={false}>
+              <FormControl
+                isInvalid={formik.touched.firstName && formik.errors.firstName}
+              >
                 <FormLabel htmlFor="firstName">Name</FormLabel>
                 <Input
                   id="firstName"
                   name="firstName"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={false}>
+              <FormControl
+                isInvalid={formik.touched.email && formik.errors.email}
+              >
                 <FormLabel htmlFor="email">Email Address</FormLabel>
                 <Input
                   id="email"
                   name="email"
                   type="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
               </FormControl>
               <FormControl>
                 <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
+                <Select
+                  id="type"
+                  name="type"
+                  value={formik.values.type}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                >
                   <option value="hireMe">Freelance project proposal</option>
                   <option value="openSource">
                     Open source consultancy session
@@ -69,16 +110,26 @@ const LandingSection = () => {
                   <option value="other">Other</option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={false}>
+              <FormControl
+                isInvalid={formik.touched.comment && formik.errors.comment}
+              >
                 <FormLabel htmlFor="comment">Your message</FormLabel>
                 <Textarea
                   id="comment"
                   name="comment"
                   height={250}
+                  value={formik.values.comment}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
+              <Button
+                type="submit"
+                backgroundColor="#CCD0CF"
+                width="full"
+                disabled={isLoading || !(formik.isValid && formik.dirty)}
+              >
                 Submit
               </Button>
             </VStack>
--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -15,24 +15,26 @@ const useSubmit = () => {
     setLoading(true);
     try {
       await wait(2000);
+      // if (random < 0.0) {
       if (random < 0.5) {
         throw new Error("Something went wrong");
       }
+      const message = `Thanks for your submission ${data.firstName}, we will get back to you shortly!`;
       setResponse({
-        type: 'success',
-        message: `Thanks for your submission ${data.firstName}, we will get back to you shortly!`,
-      })
+        type: "success",
+        message: message,
+      });
     } catch (error) {
       setResponse({
-        type: 'error',
-        message: 'Something went wrong, please try again later!',
-      })
+        type: "error",
+        message: "Something went wrong, please try again later!",
+      });
     } finally {
       setLoading(false);
     }
   };
 
   return { isLoading, response, submit };
-}
+};
 
 export default useSubmit;
